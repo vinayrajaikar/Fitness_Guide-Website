1d3f1a9dbd1879f9a8c4f880ef9f07d8090406b5
YashwantHabib <yashwanthabib567@gmail.com>
2024-06-15 17:25:03 +0530

    Added Diet page

--- fitness_guide/src/utils/FetchData.jsx
+++ fitness_guide/src/utils/FetchData.jsx
@@ -3,7 +3,7 @@ export const exerciseOptions = {
     method: 'GET',
     headers: {
       'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-      'X-RapidAPI-Key': 'b7c9488526mshdc3a21cea456dbdp1751a6jsnfc2ee1e57342',
+      'X-RapidAPI-Key': '02d665059bmsh7db8552860ff267p1a9386jsn53c48c6d4f15',
     },
   };
 
@@ -11,7 +11,7 @@ export const exerciseOptions = {
 export const YoutubeOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': 'b7c9488526mshdc3a21cea456dbdp1751a6jsnfc2ee1e57342',
+    'X-RapidAPI-Key': '02d665059bmsh7db8552860ff267p1a9386jsn53c48c6d4f15',
     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
   }
 };
