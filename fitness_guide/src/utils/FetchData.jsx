2f3f69813c0278580e728bfb6a429bafe4c5abf7
Vinayrajaikar <vinayrajaikar@gmail.com>
2024-06-25 14:41:16 +0530

    updated key

--- fitness_guide/src/utils/FetchData.jsx
+++ fitness_guide/src/utils/FetchData.jsx
@@ -3,7 +3,7 @@ export const exerciseOptions = {
     method: 'GET',
     headers: {
       'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-      'X-RapidAPI-Key': '02d665059bmsh7db8552860ff267p1a9386jsn53c48c6d4f15',
+      'X-RapidAPI-Key': '806e140286msh0279857defdcf55p12226ajsn698db7ba1be8',
     },
   };
 
@@ -11,7 +11,7 @@ export const exerciseOptions = {
 export const YoutubeOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': '02d665059bmsh7db8552860ff267p1a9386jsn53c48c6d4f15',
+    'X-RapidAPI-Key': '806e140286msh0279857defdcf55p12226ajsn698db7ba1be8',
     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
   }
 };
