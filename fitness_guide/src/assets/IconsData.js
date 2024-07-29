0c38df2b30fa752fbcf74ed62a23ad850b87b70a
Vinayrajaikar <vinayrajaikar@gmail.com>
2024-07-29 10:49:24 +0530

    Added Logos

---
+++ fitness_guide/src/assets/IconsData.js
@@ -0,0 +1,41 @@
+import back1 from '../assets/icons/back1.png'
+import arms from '../assets/icons/arms1.png'
+import chest from '../assets/icons/chest1.png'
+import legs from '../assets/icons/legs1.png'
+import cardio from '../assets/icons/cardio1.png'
+import all from '../assets/icons/gym.png'
+
+const itemImage = {
+    "back": back1,
+    "lower arms":arms,
+    "chest":chest,
+    "legs":legs,
+    "upper legs":legs,
+    "cardio": cardio,
+    "all": all,
+    "lower legs": legs,
+    "waist": chest,
+    "neck": back1,
+    "shoulders": chest,
+    "upper arms": arms
+
+    // back: "fitness_guide/src/assets/icons/back1.png",
+    // arms: "fitness_guide/src/assets/icons/arms.png",
+    // chest: "fitness_guide/src/assets/icons/chest.png",
+    // cardio: "fitness_guide/src/assets/icons/cardio.png",
+    // chest: "fitness_guide/src/assets/icons/chest.png",
+    // cardio: "C:\CODES\fitness_guideeee\fitness\fitness_guide\src\assets\icons\cardio.png"
+ };
+
+export default itemImage;
+
+
+// const itemImage = {
+//     back: require('./icons/back1.png'),
+//     cardio: require('./icons/cardio.png'),
+//     chest: require('./icons/chest.png'),
+// };
+  
+//   export default itemImage;
+  
+
--- fitness_guide/src/components/BodyPart.jsx
+++ fitness_guide/src/components/BodyPart.jsx
@@ -1,9 +1,11 @@
 import React, { useContext }  from 'react'
 import { Stack,Typography, stackClasses } from '@mui/material'
-import Icon from '../assets/icons/gym.png';
+import Icon from '../assets/icons/back.png';
+import itemImage from '../assets/IconsData.js'
 import { UserContextData } from '../App'
 
 const BodyPart = ({item}) => {
+  console.log(item);
 
   const {setBodyPart,bodyPart}=useContext(UserContextData);
   return (
@@ -18,17 +20,17 @@ const BodyPart = ({item}) => {
                 height:'280px',
                 cursor:'pointer',
                 gap:'47px',
-                // borderTop: '', // No border by default
-                // '&:hover': {
-                //   borderTop: '4px solid #ff2625',
-                // },
+                borderTop: '', // No border by default
+                '&:hover': {
+                  borderTop: '4px solid #ff2625',
+                },
             }}
             onClick={()=>{
                 setBodyPart(item);
                 window.scrollTo({top:1800,left:100,behavior:'smooth'})
             }}
             >
-        <img src={Icon} alt="dumble" style={{width:'40px',height:'40px'}}/>
+        <img src={itemImage[item]} alt="dumble" style={{width:'180px',height:'140px'}}/>
         <Typography fontSize="24px" fontWeight="bold" color="#3A1212" textTransform="capitalize">{item}</Typography>
     </Stack>
   )
